5b295c8b30f8ee147e93b2318898905ce63fc97c
Tatyana <android87@list.ru>
2022-04-26 22:38:10 +0400

    added find max

---
+++ task1_findMax/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два числа и я скажу, какое больше");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int max = numberA;
+if(numberB > max) max = numberB;
+Console.WriteLine($"среди этих двух чисел наибольшим является {max}");
