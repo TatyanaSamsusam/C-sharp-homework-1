2e5512f7ea1c0bdbe501858b5632cdb77ec40c37
Tatyana <android87@list.ru>
2022-04-26 23:04:03 +0400

    show all even numbers

---
+++ task4_showAllEven/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("введите число, и я покажу все четные числа от 1 до введенного числа");
+int number = int.Parse(Console.ReadLine());
+int evenNumber = 2;
+
+while (evenNumber < number)
+{
+    if (evenNumber % 2 == 0)
+    Console.WriteLine(evenNumber);
+    evenNumber = evenNumber +2;
+}
